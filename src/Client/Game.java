e9874ca09f2eba5976421ac0070f4cf76a6cdaf6
CirnoTheGenius <cirnothegenius@aol.com>
2012-10-25 13:54:48 -0700

    Update src/Client/Game.java

--- src/Client/Game.java
+++ src/Client/Game.java
@@ -1,6 +1,10 @@
 package Client;
 
 import java.awt.Color;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JFrame;
 
 import ClientLauncher.ClientVar;
@@ -9,8 +13,9 @@ import Server.Server;
 public class Game extends JFrame {
 
 	private JFrame main;
-	
-	public Game(Server s, ClientVar Client){
+	private final KeyboardFocusManager Keyboard = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+	private ClientVar Client;
+	public Game(Server s, ClientVar c){
 		main = new JFrame("Client");
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		main.setSize(700, 700);
@@ -18,9 +23,20 @@ public class Game extends JFrame {
 		main.setLocationRelativeTo(null);
 		main.getContentPane().setBackground(Color.DARK_GRAY);
 		
-		Chat c = new Chat(s, Client);
-		Client.setChat(c);
-		main.add(c.getContentPane());
+		Client = c;
+		
+		Keyboard.addKeyEventDispatcher(new KeyEventDispatcher(){
+			@Override
+			public boolean dispatchKeyEvent(KeyEvent key){
+				if(key.getKeyCode() == KeyEvent.VK_T && !Client.isChatting){
+					new Chat(Client);
+					Client.isChatting = true;
+				}
+				return false;
+			}
+		});
+
+		Client.setGame(this);
 	}
 	
 }
