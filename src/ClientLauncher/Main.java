b704420cdc4cb4ced71a98ac45224d57b4f4d083
CirnoTheGenius <cirnothegenius@aol.com>
2012-10-25 13:54:31 -0700

    Update src/ClientLauncher/Main.java

--- src/ClientLauncher/Main.java
+++ src/ClientLauncher/Main.java
@@ -4,6 +4,7 @@ package ClientLauncher;
  * @author Cirno the Genius/Tenko.
  */
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedInputStream;
@@ -27,10 +28,11 @@ public class Main extends JFrame implements ActionListener {
 
 	public final ClientVar Client = new ClientVar();
 	private Server s;
+	
 	private JFrame main;
 	private JTextField host, username;
+	private JLabel hostL, usernameL, picture, watermark;
 	private JButton confirm;
-	private JLabel picture, watermark;
 	
 	private final boolean debug = false;
 	
@@ -38,29 +40,42 @@ public class Main extends JFrame implements ActionListener {
 
 	public Main() throws IOException{
 		
+		//public void callMe(){}
+		//Hey, I just met you. I made a function. So here's callMe(). So call it maybe?
+		
 		InputStream sideimg = randomSideImage();
 		InputStream watermarkimg = new BufferedInputStream(getClass().getResourceAsStream("/SideImages/mark.png"));
-
+		
 		main = new JFrame("ClientLauncher");
 		main.setLayout(new FlowLayout());
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		main.setSize(700, 386);
+		main.setSize(800, 386);
 		main.setVisible(true);
 		main.setLocationRelativeTo(null);
 		main.setResizable(false);
 		
-		host = new JTextField("Host");
-		host.setSize(310, 50);
-		host.setLocation(360, 100);
+		host = new JTextField();
+		host.setSize(260, 50);
+		host.setLocation(510, 100);
 		
-		username = new JTextField("username");
-		username.setSize(310, 50);
-		username.setLocation(360, 160);
+		hostL = new JLabel("Host: ");
+		hostL.setSize(70, 20);
+		hostL.setLocation(430, 115);
+		hostL.setFont(new Font(hostL.getFont().getFontName(), Font.BOLD, 25));
+
+		username = new JTextField();
+		username.setSize(260, 50);
+		username.setLocation(510, 160);
+		
+		usernameL = new JLabel("Username: ");
+		usernameL.setSize(200, 20);
+		usernameL.setLocation(365, 170);
+		usernameL.setFont(new Font(usernameL.getFont().getFontName(), Font.BOLD, 25));
 		
 		confirm = new JButton("Connect!");
 		confirm.addActionListener(this);
 		confirm.setSize(155, 50);
-		confirm.setLocation(440, 220);
+		confirm.setLocation(560, 220);
 
 		picture = new JLabel();
 		picture.setIcon(new ImageIcon(ImageIO.read(sideimg)));
@@ -70,20 +85,21 @@ public class Main extends JFrame implements ActionListener {
 		watermark = new JLabel();
 		watermark.setIcon(new ImageIcon(ImageIO.read(watermarkimg)));
 		watermark.setSize(100, 89);
-		watermark.setLocation(600, 269);
-		
-		main.getContentPane().add(picture);
-		main.getContentPane().add(host);
-		main.getContentPane().add(username);
-		main.getContentPane().add(confirm);
-		main.getContentPane().add(watermark);
+		watermark.setLocation(700, 269);
+
+		main.add(picture);
+		main.add(host);
+		main.add(username);
+		main.add(confirm);
+		main.add(watermark);
+		main.add(hostL);
+		main.add(usernameL);
 		main.repaint();
 		
 		sideimg.close();
 		watermarkimg.close();
 	}
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) throws IOException{
 		new Main();
 	}
@@ -117,5 +133,5 @@ public class Main extends JFrame implements ActionListener {
 			e.printStackTrace();
 		}
 	}
-
+	
 }
